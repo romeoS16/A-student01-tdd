3bad59cecb70e39ee70c2842f0f0581f18b52d60
romeoS16 <romeoschweizer29@gmail.com>
2025-12-09 17:46:18 +0100

    aufgaben

---
+++ src/test/java/de/ba/experiment/password/PasswordStrengthEvaluatorTest.java
@@ -0,0 +1,20 @@
+package de.ba.experiment.password;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordStrengthEvaluatorTest {
+
+    // In dieser Gruppe arbeiten Sie mit Test-Driven Development (TDD).
+    // Schreiben Sie Tests zuerst, bevor Sie die Methode implementieren
+    // oder ändern (Red – Green – Refactor).
+
+    @Test
+    public void nullPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength(null);
+        assertEquals("WEAK", result);
+    }
+
+    // TODO: weitere Tests ergänzen
+}
