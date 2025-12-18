11a4c476fc60197b6d29e787a06e1819fae76a08
jonez187 <147058994+jonez187@users.noreply.github.com>
2025-12-18 12:56:43 +0100

    Aufgabe 3: Passwords

--- src/main/java/de/ba/experiment/password/PasswordStrengthEvaluator.java
+++ src/main/java/de/ba/experiment/password/PasswordStrengthEvaluator.java
@@ -1,5 +1,7 @@
 package de.ba.experiment.password;
 
+import java.util.regex.Pattern;
+
 public class PasswordStrengthEvaluator {
 
     /**
@@ -11,8 +13,28 @@ public class PasswordStrengthEvaluator {
      * @return "WEAK", "MEDIUM" oder "STRONG" nach Definition
      */
 
+
+    private static final Pattern[] inputRegexes = new Pattern[5];
+
+    static {
+        inputRegexes[0] = Pattern.compile(".*[A-Z].*");                                                     // Upper Case
+        inputRegexes[1] = Pattern.compile(".*[a-z].*");                                                     // Lower Case
+        inputRegexes[2] = Pattern.compile(".*[0-9].*");                                                     // Ziffer
+        inputRegexes[3] = Pattern.compile(".*[`~!@#$%^&*()\\-_=+\\\\|\\[{\\]};:'\",<.>/?].*");              // Sonderzeichen
+        inputRegexes[4] = Pattern.compile("[a-zA-Z0-9`~!@#$%^&*()\\-_=+\\\\|\\[{\\]};:'\",<.>/?]{10,}");    // Länge >= 10
+    }
+
     public static String evaluateStrength(String password) {
-        // TODO: implementieren
-        return "WEAK";
+        if (password == null || password.length() < 6 || password.contains(" ")) return "WEAK";
+
+        int points = 0;
+
+        for (Pattern inputRegex : inputRegexes) {
+            if (inputRegex.matcher(password).matches()) points += 1;
+        }
+
+        if (points <= 2) return "WEAK";
+        else if (points == 3) return "MEDIUM";
+        else return "STRONG";
     }
 }
--- src/test/java/de/ba/experiment/password/PasswordStrengthEvaluatorTest.java
+++ src/test/java/de/ba/experiment/password/PasswordStrengthEvaluatorTest.java
@@ -10,11 +10,114 @@ public class PasswordStrengthEvaluatorTest {
     // Schreiben Sie Tests zuerst, bevor Sie die Methode implementieren
     // oder ändern (Red – Green – Refactor).
 
+
+    // WEAK PASSWORD TESTS
     @Test
     public void nullPassword_isWeak() {
-        String result = PasswordStrengthEvaluator.evaluateStrength("ab1");
+        String result = PasswordStrengthEvaluator.evaluateStrength(null);
         assertEquals("WEAK", result);
     }
 
-    // TODO: weitere Tests ergänzen
+    @Test
+    public void emptyStringPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void containsSpace_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("AAab 12112!!");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void shortPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("abaA1");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void onlyLowerCase_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("nurlowercasewermachtsowas");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void onlyUpperCase_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("WIESERIÖSMUSSICHBLEIBEN");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void onlyNumbers_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("676767676767187");
+        assertEquals("WEAK", result);
+    }
+
+
+
+
+    // MEDIUM PASSWORD TESTS
+    @Test
+    public void mediumPasswordUpperLowerCaseNumber() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("abaAa1");
+        assertEquals("MEDIUM", result);
+    }
+
+    @Test
+    public void mediumPasswordUpperLowerCaseSpecialChar() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("abaAa.");
+        assertEquals("MEDIUM", result);
+    }
+
+    @Test
+    public void mediumPasswordUpperLowerCaseLong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("abaAbabsabaaaaababa");
+        assertEquals("MEDIUM", result);
+    }
+
+    @Test
+    public void mediumPasswordUpperLowerCaseLongExactTen() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("abaAbabsab");
+        assertEquals("MEDIUM", result);
+    }
+
+    @Test
+    public void mediumPasswordLowerCaseNumberSpecialChar() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("a1a1....");
+        assertEquals("MEDIUM", result);
+    }
+
+    @Test
+    public void mediumPasswordLowerCaseNumberLong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("a1a111111111111111");
+        assertEquals("MEDIUM", result);
+    }
+
+    // STRONG PASSWORD TESTS
+    @Test
+    public void strongPasswordNoSpecialChar() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aA1aA111111111111111");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void strongPasswordNotLong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aA1aA11!");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void strongPasswordNoLowerCase() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("AAAAAAA111!!!");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void strongPasswordNoUpperCase() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aaaaaaaaaaaaa111!!!");
+        assertEquals("STRONG", result);
+    }
+
+
 }
